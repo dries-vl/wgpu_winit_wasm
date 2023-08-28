6a37a3393da22dfaa0db6799a675eb064b7b2b8a
dries-vl <driesvanleuvenhaege@hotmail.com>
2023-08-28 09:44:35 +0200

    add wgpu basics to webassembly

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,4 @@ fn main() {
     // set up logging
     env_logger::init();
 
-    // create window and event loop
-    let (event_loop, window) = windowing::init_window();
-
-    // start window event loop
-    windowing::run_event_loop(event_loop, window);
 }
