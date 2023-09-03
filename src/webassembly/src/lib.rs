9241823ce073112ad2ecd2184283cca4a712c8d1
dries-vl <driesvanleuvenhaege@hotmail.com>
2023-09-03 13:47:31 +0200

    minor fix: remove some comments

--- src/webassembly/src/lib.rs
+++ src/webassembly/src/lib.rs
@@ -569,8 +569,8 @@ impl State {
             depth_stencil: Some(wgpu::DepthStencilState {
                 format: texture::Texture::DEPTH_FORMAT,
                 depth_write_enabled: true,
-                depth_compare: wgpu::CompareFunction::Less, // 1.
-                stencil: wgpu::StencilState::default(), // 2.
+                depth_compare: wgpu::CompareFunction::Less,
+                stencil: wgpu::StencilState::default(),
                 bias: wgpu::DepthBiasState::default(),
             }),
             multisample: wgpu::MultisampleState {
